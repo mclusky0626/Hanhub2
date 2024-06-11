4703903138643a5e645342195070563e8686e7fe
mclusky0626 <102368729+mclusky0626@users.noreply.github.com>
2024-06-11 21:11:52 +0900

    sad
    sad

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB 제한
+
+# 업로드 폴더가 없으면 생성
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+def get_uploaded_files():
+    files = []
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if os.path.isfile(path):
+            files.append((filename, datetime.fromtimestamp(os.path.getctime(path))))
+    files.sort(key=lambda x: x[1], reverse=True)
+    return files
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+
+@app.route('/uploaded_images')
+def uploaded_images():
+    files = get_uploaded_files()
+    return render_template('uploaded_images.html', files=files)
+
+if __name__ == "__main__":
+    app.run(debug=True)
